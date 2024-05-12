0e52cfc9940870aca2e5d2a94fcdb8c9973c3552
Manutama aditya <55338832+kmnvz-mayvez@users.noreply.github.com>
2024-05-12 12:23:14 +0700

    add spark

--- _script/json.py
+++ _script/json.py
@@ -4,9 +4,9 @@ file_path = r'C:\APPBG'
 with open(file_path, 'r') as file:
     lines = file.readlines()
 data = {'plates': []}
-for line in lines:
+for idx, line in enumerate(lines, 1):
     plate = line.strip()
-    data['plates'].append({'plate': plate, 'source': 'motorcycle'})
+    data['plates'].append({'id': idx, 'plate': plate, 'source': 'motorcycle'})
 
 json_file_path = r'C:\APPBG'
 with open(json_file_path, 'w') as json_file:
