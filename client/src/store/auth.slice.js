a144a9539b712f5f8aad4aba5fdcb806bbb0cf8f
Manutama aditya <55338832+kmnvz-mayvez@users.noreply.github.com>
2024-05-13 07:36:36 +0700

    add demo-client

---
+++ client/src/store/auth.slice.js
@@ -0,0 +1,70 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+
+import { alertActions } from 'store';
+import { history, fetchWrapper } from 'helpers';
+
+const name = 'auth';
+const initialState = createInitialState();
+const reducers = createReducers();
+const extraActions = createExtraActions();
+const slice = createSlice({ name, initialState, reducers });
+
+export const authActions = { ...slice.actions, ...extraActions };
+export const authReducer = slice.reducer;
+
+function createInitialState() {
+    return {
+        value: JSON.parse(localStorage.getItem('auth'))
+    }
+}
+
+function createReducers() {
+    return {
+        setAuth
+    };
+
+    function setAuth(state, action) {
+        state.value = action.payload;
+    }
+}
+
+function createExtraActions() {
+    const baseUrl = `${process.env.REACT_APP_API_URL}/users`;
+
+    return {
+        login: login(),
+        logout: logout()
+    };
+
+    function login() {
+        return createAsyncThunk(
+            `${name}/login`,
+            async function ({ username, password }, { dispatch }) {
+                dispatch(alertActions.clear());
+                try {
+                    const user = await fetchWrapper.post(`${baseUrl}/authenticate`, { username, password });
+
+                    dispatch(authActions.setAuth(user));
+
+                    localStorage.setItem('auth', JSON.stringify(user));
+
+                    const { from } = history.location.state || { from: { pathname: '/' } };
+                    history.navigate(from);
+                } catch (error) {
+                    dispatch(alertActions.error(error));
+                }
+            }
+        );
+    }
+
+    function logout() {
+        return createAsyncThunk(
+            `${name}/logout`,
+            function (arg, { dispatch }) {
+                dispatch(authActions.setAuth(null));
+                localStorage.removeItem('auth');
+                history.navigate('/account/login');
+            }
+        );
+    }
+}
---
+++ client/src/store/users.slice.js
@@ -0,0 +1,134 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+
+import { authActions } from 'store';
+import { fetchWrapper } from 'helpers';
+
+const name = 'users';
+const initialState = createInitialState();
+const extraActions = createExtraActions();
+const extraReducers = createExtraReducers();
+const slice = createSlice({ name, initialState, extraReducers });
+
+export const userActions = { ...slice.actions, ...extraActions };
+export const usersReducer = slice.reducer;
+
+function createInitialState() {
+    return {
+        list: null,
+        item: null
+    }
+}
+
+function createExtraActions() {
+    const baseUrl = `${process.env.REACT_APP_API_URL}/users`;
+
+    return {
+        register: register(),
+        getAll: getAll(),
+        getById: getById(),
+        update: update(),
+        delete: _delete()
+    };
+
+    function register() {
+        return createAsyncThunk(
+            `${name}/register`,
+            async (user) => await fetchWrapper.post(`${baseUrl}/register`, user)
+        );
+    }
+
+    function getAll() {
+        return createAsyncThunk(
+            `${name}/getAll`,
+            async () => await fetchWrapper.get(baseUrl)
+        );
+    }
+
+    function getById() {
+        return createAsyncThunk(
+            `${name}/getById`,
+            async (id) => await fetchWrapper.get(`${baseUrl}/${id}`)
+        );
+    }
+
+    function update() {
+        return createAsyncThunk(
+            `${name}/update`,
+            async function ({ id, data }, { getState, dispatch }) {
+                await fetchWrapper.put(`${baseUrl}/${id}`, data);
+
+                const auth = getState().auth.value;
+                if (id === auth?.id.toString()) {
+                    const user = { ...auth, ...data };
+                    localStorage.setItem('auth', JSON.stringify(user));
+
+                    dispatch(authActions.setAuth(user));
+                }
+            }
+        );
+    }
+    function _delete() {
+        return createAsyncThunk(
+            `${name}/delete`,
+            async function (id, { getState, dispatch }) {
+                await fetchWrapper.delete(`${baseUrl}/${id}`);
+                
+                if (id === getState().auth.value?.id) {
+                    dispatch(authActions.logout());
+                }
+            }
+        );
+    }
+}
+
+function createExtraReducers() {
+    return (builder) => {
+        getAll();
+        getById();
+        _delete();
+
+        function getAll() {
+            var { pending, fulfilled, rejected } = extraActions.getAll;
+            builder
+                .addCase(pending, (state) => {
+                    state.list = { loading: true };
+                })
+                .addCase(fulfilled, (state, action) => {
+                    state.list = { value: action.payload };
+                })
+                .addCase(rejected, (state, action) => {
+                    state.list = { error: action.error };
+                });
+        }
+
+        function getById() {
+            var { pending, fulfilled, rejected } = extraActions.getById;
+            builder
+                .addCase(pending, (state) => {
+                    state.item = { loading: true };
+                })
+                .addCase(fulfilled, (state, action) => {
+                    state.item = { value: action.payload };
+                })
+                .addCase(rejected, (state, action) => {
+                    state.item = { error: action.error };
+                });
+        }
+
+        function _delete() {
+            var { pending, fulfilled, rejected } = extraActions.delete;
+            builder
+                .addCase(pending, (state, action) => {
+                    const user = state.list.value.find(x => x.id === action.meta.arg);
+                    user.isDeleting = true;
+                })
+                .addCase(fulfilled, (state, action) => {
+                    state.list.value = state.list.value.filter(x => x.id !== action.meta.arg);
+                })
+                .addCase(rejected, (state, action) => {
+                    const user = state.list.value.find(x => x.id === action.meta.arg);
+                    user.isDeleting = false;
+                });
+        }
+    }
+}
