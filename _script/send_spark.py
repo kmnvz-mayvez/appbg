5d8430a1a045ccad6f5f8f93d6ae38fb763a7757
Manutama aditya <55338832+kmnvz-mayvez@users.noreply.github.com>
2024-05-18 10:07:39 +0700

    update python script

---
+++ _script/send_spark.py
@@ -0,0 +1,35 @@
+from pyspark.sql import SparkSession
+
+# Membuat Spark session
+spark = SparkSession.builder \
+    .appName("Elasticsearch Integration") \
+    .config("spark.jars.packages", "org.elasticsearch:elasticsearch-spark-30_2.12:8.0.0") \
+    .config("spark.es.nodes", "localhost") \
+    .config("spark.es.port", "9200") \
+    .config("spark.es.net.http.auth.user", "") \
+    .config("spark.es.net.http.auth.pass", "") \
+    .config("spark.es.nodes.wan.only", "false") \
+    .config("spark.driver.extraJavaOptions", "-Dlog4j.configuration=file:log4j.properties") \
+    .config("spark.executor.extraJavaOptions", "-Dlog4j.configuration=file:log4j.properties") \
+    .getOrCreate()
+
+# Membaca file CSV pertama (data-user.csv)
+df_user = spark.read.csv("file:///_script/_csv/data-user.csv", header=True, inferSchema=True)
+
+# Membaca file CSV kedua (plat.csv)
+df_plat = spark.read.csv("file:///_script/_csv/data-plates.csv", header=True, inferSchema=True)
+
+# Mengubah nama kolom
+df_user = df_user.withColumnRenamed("_id", "id")
+df_user = df_user.withColumnRenamed("plateNumber", "plate")
+df_plat = df_plat.withColumnRenamed("_id", "id")
+
+# Menggabungkan DataFrame
+df_merge = df_user.join(df_plat, "plate")
+
+# Menyimpan DataFrame ke Elasticsearch
+df_merge.write \
+    .format("org.elasticsearch.spark.sql") \
+    .option("es.resource", "merge/plates") \
+    .mode("overwrite") \
+    .save()
